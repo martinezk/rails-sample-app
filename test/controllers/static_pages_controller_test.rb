b461f20a7940128c6553b1b1a07c79ffb432d225
martinezk <misskristinamartinez@gmail.com>
2018-04-11 12:59:31 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get :home
+    assert_response :success
+  end
+
   test "should get home" do
     get :home
     assert_response :success
